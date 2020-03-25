7dcd5d3f0f5c717eca70e455626672b3bd922e4c
mahajant99 <mahajant99@gmail.com>
2020-03-25 17:47:28 +0530

    sakura: prop: Update Prop
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -8,13 +8,6 @@ ro.vendor.audio.sdk.fluencetype=fluence
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-persist.camera.dual.camera=0
-
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=256m \
-dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
-dalvik.vm.heapmaxfree=8m
+persist.camera.dual.camera=0 \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1
