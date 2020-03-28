b2125055678f5403c82861aa789b815f09c0d16d
mahajant99 <mahajant99@gmail.com>
2020-03-28 16:27:37 +0530

    sakura: Update Blobs From Daisy Q * Updated From daisy-user-10-QKQ1.191002.002-V11.0.2.0.QDLMIXM-release-keys * Updated Camera Blobs * Updated Consumer IR Blobs * Updated Fingerprint Blobs
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -9,5 +9,5 @@ ro.vendor.audio.sdk.fluencetype=fluence
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0 \
-persist.camera.HAL3.enabled=1 \
+persist.vendor.camera.HAL3.enabled=1 \
 persist.camera.eis.enable=1
